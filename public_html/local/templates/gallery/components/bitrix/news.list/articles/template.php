64f7db4e9fe3e3c5c4d119aa89b0e8a8726bd6b7
dayAlone <andrey.slider@gmail.com>
2015-08-08 02:51:03 +0500

    Articles

--- public_html/local/templates/gallery/components/bitrix/news.list/articles/template.php
+++ public_html/local/templates/gallery/components/bitrix/news.list/articles/template.php
@@ -4,10 +4,10 @@
         <div class="col-lg-3 col-sm-4">
           <div class="article article--first">
             <div class="row">
-              <div class="col-xs-5 col-sm-12"><a href="#Modal" data-toggle="modal" style="background-image:url(<?=$item['PREVIEW_PICTURE']['SRC']?>);filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=$item['PREVIEW_PICTURE']['SRC']?>', sizingMethod='scale');-ms-filter: &quot;progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=$item['PREVIEW_PICTURE']['SRC']?>', sizingMethod='scale')&quot;;" class="article__image"></a></div>
-              <div class="col-xs-7 col-sm-12"><a href="#Modal" data-toggle="modal" class="article__title"><?=$item['NAME']?></a>
+              <div class="col-xs-5 col-sm-12"><a data-url="<?=$item['PROPERTIES']['LINK']['VALUE']?>" href="#Modal" data-toggle="modal" style="background-image:url(<?=$item['PREVIEW_PICTURE']['SRC']?>);filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=$item['PREVIEW_PICTURE']['SRC']?>', sizingMethod='scale');-ms-filter: &quot;progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=$item['PREVIEW_PICTURE']['SRC']?>', sizingMethod='scale')&quot;;" class="article__image"></a></div>
+              <div class="col-xs-7 col-sm-12"><a data-url="<?=$item['PROPERTIES']['LINK']['VALUE']?>" href="#Modal" data-toggle="modal" class="article__title"><?=$item['NAME']?></a>
                 <div class="page__divider"></div>
-                <p class="article__text"><?=$item['PREVIEW_TEXT<']?></p>
+                <p class="article__text"><?=$item['PREVIEW_TEXT']?></p>
               </div>
             </div>
           </div>
--- public_html/local/templates/gallery/footer.php
+++ public_html/local/templates/gallery/footer.php
@@ -1,3 +1,10 @@
 </div>
+<div id="Modal" tabindex="-1" role="dialog" aria-hidden="true" data-parsley-validate class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content"><a data-dismiss="modal" href="#" class="close"></a>
+      <div class="text"></div>
+    </div>
+  </div>
+</div>
 <div class="overlay"></div></body>
 </html>
