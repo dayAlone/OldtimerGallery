ce82f74aafba54082cd86d52cb0a760300202090
dayAlone <andrey.slider@gmail.com>
2015-08-08 01:08:38 +0500

    Index

--- public_html/index.php
+++ public_html/index.php
@@ -9,15 +9,13 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
             <p class="promo__text">Легенды автоспорта – вновь на старте. Рёв их прямоточных выхлопов заставит сердца учащённо забиться. Незабываемое зрелище – чёрно-белая кинохроника оживёт на Ваших глазах.</p><br>
             <div class="promo__divider promo__divider--white"></div>
           </div>
-      </a>
-      <a href="/paddok/" style="background-image:url(/layout/images/paddock-bg-inside.jpg)" class="promo promo--blue"><img src="/layout/images/person-paddock.png" class="promo__image">
+      </a><a href="/paddok/" style="background-image:url(/layout/images/paddock-bg-inside.jpg)" class="promo promo--blue"><img src="/layout/images/person-paddock.png" class="promo__image">
           <div class="promo__content">
             <h2 class="promo__title">паддок</h2>
             <p class="promo__text">Побывать в паддоке гоночной команды 1950-х годов. Увидеть, как прогревают, заправляют, ремонтируют и готовят к показательным заездам десятки уникальных суперкаров.</p><br>
             <div class="promo__divider"></div>
           </div>
-      </a>
-      <a href="/gallery/" style="background-image: url(/layout/images/vystavka-bg-inside.jpg)" class="promo promo--orange"><img src="/layout/images/person-exhibition.png" class="promo__image">
+      </a><a href="/gallery/" style="background-image: url(/layout/images/vystavka-bg-inside.jpg)" class="promo promo--orange"><img src="/layout/images/person-exhibition.png" class="promo__image">
           <div class="promo__content">
             <h2 class="promo__title">выставка</h2>
             <p class="promo__text">В 24-й раз автореставраторы представят свои достижения – громкие премьеры, сюрпризы и сенсации. Развалы «блошиного рынка» также достойны внимания любителей ретро.</p><br>
