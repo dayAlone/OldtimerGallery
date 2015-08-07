992a4c329d6d959d537e5cc60a9a9252b904392e
dayAlone <andrey.slider@gmail.com>
2015-08-08 02:08:55 +0500

    Index

--- public_html/local/templates/gallery/components/bitrix/news.list/promos/template.php
+++ public_html/local/templates/gallery/components/bitrix/news.list/promos/template.php
@@ -3,14 +3,13 @@
         $bg = $item['DETAIL_PICTURE']['SRC'];
         if(intval($item['PROPERTIES']['BG']['VALUE'])>0)
             $bg = CFile::GetPath($item['PROPERTIES']['BG']['VALUE']);
-        ?>
-        <a href="/<?=$item['CODE']?>/" style="background-image: url(<?=$bg?>)" class="promo promo--<?=$item['PROPERTIES']['CLASS']['VALUE']?>">
+        ?><a href="/<?=$item['CODE']?>/" style="background-image: url(<?=$bg?>)" class="promo promo--<?=$item['PROPERTIES']['CLASS']['VALUE']?>">
             <img src="<?=$item['PREVIEW_PICTURE']['SRC']?>" class="promo__image">
             <div class="promo__content">
               <h2 class="promo__title"><?=$item['NAME']?></h2>
               <p class="promo__text"><?=$item['PREVIEW_TEXT']?></p><br>
               <div class="promo__divider promo__divider--white"></div>
             </div>
-        </a>
-    <?endforeach?>
+        </a><?
+    endforeach?>
 </div>
