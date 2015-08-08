f6db843a3a758ec15796bacb2e7d5c9a347f5fd0
dayAlone <andrey.slider@gmail.com>
2015-08-08 15:18:17 +0500

    Quotes

--- public_html/gallery/index.php
+++ public_html/gallery/index.php
@@ -41,32 +41,21 @@ $APPLICATION->SetTitle("Выставка");
       <h2 class="page__title">отзывы</h2>
       <div class="page__divider"></div>
     </div>
-    <div class="quotes">
-      <div data-bg="#d27700" data-border="#ffc000" class="quote">
-        <div class="quote__frame">
-          <div class="quote__arrow"><svg width="55" height="42" viewBox="0 0 55 42" xmlns="http://www.w3.org/2000/svg"><g id="Page-1" fill="none" fill-rule="evenodd"><g id="corner"><path d="M5.5 42c0-1.11 1.054-3.252 2.188-4.124L48.524 6.484l5.83-4.482H7.002C6.172 2.002 5.5 1.332 5.5 0H0v42h5.5z" id="bg"/><path d="M5.5 42c0-1.11 1.054-3.252 2.188-4.124L48.524 6.484l5.83-4.482H7.002C6.172 2.002 5.5 1.332 5.5 0h-5c0 4.093 2.91 7.002 6.502 7.002H39.5L4.64 33.912C2.275 35.732.5 39.337.5 42h5z" id="arrow"/></g></g></svg>
-          </div>
-          <p>«Посетив Олдтаймер-Галерею, я понял, что автомобили середины двадцатого века — самые любопытные с точки зрения человеческого таланта изобретать, совершенствовать и удивлять. Есть такие образцы, в которых остановилось и отразилось время, как свидетельство человеческого гения и вкуса. Это как старинное вино — надо его уметь отличать, чувствовать и наслаждаться»</p>
-        </div>
-        <div class="quote__author">
-          <div style="background-url(/layout/images/pic.jpg)" class="quote__image"></div>
-          <div class="quote__name">Леонид Ярмольник</div>
-          <div class="quote__info">актер</div>
-        </div>
-      </div>
-      <div data-bg="#d9916b" data-border="#a6001a" class="quote">
-        <div class="quote__frame">
-          <div class="quote__arrow"><svg width="55" height="42" viewBox="0 0 55 42" xmlns="http://www.w3.org/2000/svg"><g id="Page-1" fill="none" fill-rule="evenodd"><g id="corner"><path d="M5.5 42c0-1.11 1.054-3.252 2.188-4.124L48.524 6.484l5.83-4.482H7.002C6.172 2.002 5.5 1.332 5.5 0H0v42h5.5z" id="bg"/><path d="M5.5 42c0-1.11 1.054-3.252 2.188-4.124L48.524 6.484l5.83-4.482H7.002C6.172 2.002 5.5 1.332 5.5 0h-5c0 4.093 2.91 7.002 6.502 7.002H39.5L4.64 33.912C2.275 35.732.5 39.337.5 42h5z" id="arrow"/></g></g></svg>
-          </div>
-          <p>«Посетив Олдтаймер-Галерею, я понял, что автомобили середины двадцатого века — самые любопытные с точки зрения человеческого таланта изобретать, совершенствовать и удивлять. Есть такие образцы, в которых остановилось и отразилось время, как свидетельство человеческого гения и вкуса. Это как старинное вино — надо его уметь отличать, чувствовать и наслаждаться»</p>
-        </div>
-        <div class="quote__author">
-          <div style="background-url(/layout/images/pic.jpg)" class="quote__image"></div>
-          <div class="quote__name">Леонид Ярмольник</div>
-          <div class="quote__info">актер</div>
-        </div>
-      </div>
-    </div>
+    <?
+		$APPLICATION->IncludeComponent("bitrix:news.list", "quotes",
+			array(
+				"IBLOCK_ID"            => 41,
+				"NEWS_COUNT"           => "1000",
+				"SORT_BY1"             => "SORT",
+				"SORT_ORDER1"          => "ASC",
+				"CACHE_TYPE"           => "A",
+				'PROPERTY_CODE'        => array('ABOUT', 'BORDER', 'BG'),
+				"SET_TITLE"            => "N"
+			),
+			false
+		);
+	?>
+
     <div class="center xxl-margin-top xxl-padding-top"><a href="/buy.html" class="button">увидеть своими глазами</a></div>
   </div>
 </div>
