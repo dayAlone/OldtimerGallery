6e730bf03122213cae901b248821a977a1a3c7fc
dayAlone <andrey.slider@gmail.com>
2015-08-12 18:59:53 +0500

    Gallery

--- public_html/local/templates/gallery/components/bitrix/news.detail/gallery/template.php
+++ public_html/local/templates/gallery/components/bitrix/news.detail/gallery/template.php
@@ -1,7 +1,7 @@
 <?$item = $arResult;?>
 
 <div class="gallery">
-    <? foreach($item['PROPERTIES']['BUTTONS']['VALUE'] as $image):?>
+    <? foreach($item['PROPERTIES']['IMAGES']['VALUE'] as $image):?>
     <div class="gallery__item" style="background: url(<?=CFile::GetPath($image)?>)"></div>
     <?endforeach;?>
 </div>
